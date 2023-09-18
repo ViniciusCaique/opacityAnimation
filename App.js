052ee0c5a07b479af3a465ca8f9404d9dfa128e3
Vinicius Caique <viniciuscaiquesilva@gmail.com>
2023-09-18 09:37:01 -0300

    opacity

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 export default function App() {
+
+  const textOpacity = useSharedValue(0)
+
+  const opacity = useAnimatedStyle(() => {
+    return{
+      opacity: textOpacity.value
+    }
+  })
+
+  function changeOpacity() {
+    return(
+      textOpacity.value = withTiming(1, { duration: 1500 })
+    )
+  }
+
+  function changeOpacityAgain() {
+    return(
+      textOpacity.value = withTiming(0, { duration: 1500 })
+    )
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>teste</Text>
+      <Animated.Text style={[ opacity ]}>teste</Animated.Text>
+      <TouchableOpacity
+        onPress={changeOpacity}
+      >
+        <Text>Mostrar texto</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={changeOpacityAgain}
+      >
+        <Text>Esconder texto</Text>
+      </TouchableOpacity>
     </View>
   );
 }
